98bcccebafbe9c1f748a0347a57b44f6df7278a0
Daniel Kreling <dbkreling@gmail.com>
2017-08-30 07:56:16 -0300

    auth: create a social authentication method
    This commit introduces a way of register and authenticate to the bookstore
    via facebook, using social apps.
    
    Ref.: Section 3, Lecture 13 - Module 3.4
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social.apps.django_app.default',
     'registration',
     'store',
 )
@@ -65,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.apps.django_app.context_processors.backends',
+                'social.apps.django_app.context_processors.login_redirect',
             ],
         },
     },
@@ -72,6 +75,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookstore.wsgi.application'
 
+AUTHENTICATION_BACKENDS = [
+    'social.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend'
+]
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
@@ -116,3 +123,7 @@ EMAIL_HOST_PASSWORD = "sopaderoda"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "books@misterybookstore.com"
+
+# Social Auth - Facebook
+SOCIAL_AUTH_FACEBOOK_KEY = '189618651579342'
+SOCIAL_AUTH_FACEBOOK_SECRET = '8a6cc617f140fc2ec6e98a054b87395e'
