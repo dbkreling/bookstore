a268e6555a49845a737275e50490af0eb6a67a36
Daniel Kreling <dbkreling@gmail.com>
2017-09-06 07:59:15 -0300

    storefront: add new detail page for books
    Add author and reviews modules. Add a cover and detail page for books.
    Also changed admin template to reflect these changes accordingly.
    
    Ref.: Section 5, Lecture 19 - Module 5.2B
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- store/views.py
+++ store/views.py
@@ -11,3 +11,9 @@ def store(request):
         'books': books,
     }
     return render(request, 'base.html', context)
+
+def book_details(request, book_id):
+    context = {
+        'book': Book.objects.get(pk=book_id),
+    }
+    return render(request, 'store/detail.html', context)
